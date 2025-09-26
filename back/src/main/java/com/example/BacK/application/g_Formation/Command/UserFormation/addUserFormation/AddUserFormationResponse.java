2ac7eade45e92964aacf2916b3c90ce562ba2623
AnasWahada07 <anas.wahada@esprit.tn>
2025-09-26 16:27:42 +0100

    modifier des classes

--- back/src/main/java/com/example/BacK/application/g_Formation/Command/UserFormation/addUserFormation/AddUserFormationResponse.java
+++ back/src/main/java/com/example/BacK/application/g_Formation/Command/UserFormation/addUserFormation/AddUserFormationResponse.java
@@ -1,7 +1,6 @@
 package com.example.BacK.application.g_Formation.Command.UserFormation.addUserFormation;
 
 
-import com.example.BacK.application.models.UserFormationDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddUserFormationResponse {
-    private  String id;
-    private UserFormationDTO userFormation;
+    private String id;
 
-
-    public AddUserFormationResponse(UserFormationDTO dto) {
-    }
 }
